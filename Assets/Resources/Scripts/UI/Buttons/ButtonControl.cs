28d19ca4b65d4cf0cc543220fbf2d599b6470aae
Stephanie Owen <stephanie@90ninecents.com>
2012-11-22 11:23:26 -0800

    Update for mobile build

--- Assets/Resources/Scripts/UI/Buttons/ButtonControl.cs
+++ Assets/Resources/Scripts/UI/Buttons/ButtonControl.cs
@@ -3,39 +3,54 @@ using System.Collections;
 
 public class ButtonControl : MonoBehaviour {
 	// Handles touch events on child Button objects
+	bool dragging = false;
 
 	void OnEnable() {
 		Gesture.onTouchUpE += OnTouch;
+		Gesture.onDraggingE += OnDrag;
+		Gesture.onDraggingEndE += OnDragEnd;
 	}
 	
 	void OnDisable() {
 		Gesture.onTouchUpE -= OnTouch;
+		Gesture.onDraggingE += OnDrag;
+		Gesture.onDraggingEndE -= OnDragEnd;
+	}
+	
+	void OnDrag(DragInfo di) {
+		dragging = true;
+	}
+	void OnDragEnd(Vector2 touchPos) {
+		dragging = false;
 	}
 	
 	void OnTouch (Vector2 touchPos) {
-		
-		foreach (Transform t in transform) {
-			if (t.GetComponent<Button>() != null) {
-				
-				if (t.renderer != null) {
-					if (t.renderer.bounds.Contains(new Vector3(touchPos.x-Screen.width/2, touchPos.y-Screen.height/2, t.position.z))) {
-						t.GetComponent<Button>().Fire();
-						break;
+		if (!dragging) {
+			
+			foreach (Transform t in transform) {
+				if (t.GetComponent<Button>() != null) {
+					
+					if (t.renderer != null) {
+						if (t.renderer.bounds.Contains(new Vector3(touchPos.x-Screen.width/2, touchPos.y-Screen.height/2, t.position.z))) {
+							t.GetComponent<Button>().Fire();
+							break;
+						}
 					}
-				}
-				else if (t.guiTexture != null) {
-					if (t.guiTexture.GetScreenRect().Contains(touchPos)) {
-						t.GetComponent<Button>().Fire();
-						break;
+					else if (t.guiTexture != null) {
+						if (t.guiTexture.GetScreenRect().Contains(touchPos)) {
+							t.GetComponent<Button>().Fire();
+							break;
+						}
 					}
-				}
-				else if (t.guiText != null) {
-					if (t.guiText.GetScreenRect().Contains(touchPos)) {
-						t.GetComponent<Button>().Fire();
-						break;
+					else if (t.guiText != null) {
+						if (t.guiText.GetScreenRect().Contains(touchPos)) {
+							t.GetComponent<Button>().Fire();
+							break;
+						}
 					}
 				}
 			}
+			
 		}
 	}
 }
